31e5b3082b0494427d12355d99b335e1d2b922e6
taiolamz <hassanadefarato@gmail.com>
2024-07-18 04:42:54 +0100

    fix: fixed kep props

--- src/components/DashboardTableHead.tsx
+++ src/components/DashboardTableHead.tsx
@@ -7,13 +7,14 @@ import Image from "next/image";
 const DashboardTableHead = () => {
   return (
     <thead className="flex items-center px-5">
-      <Checkbox id="terms1" className="border-[#CBD5E1] border rounded-[5px] h-6 w-6"  />
+      <Checkbox
+        id="terms1"
+        className="border-[#CBD5E1] border rounded-[5px] h-6 w-6"
+      />
       <tr className="grid grid-cols-4 w-full gap-40 items-center p-5">
         {dashboardTableHeadData.map((chi, idx) => (
-          <tr className="flex gap-2 items-center">
-            <td key={idx} className="text-[#334155] text-xs font-normal">
-              {chi}
-            </td>
+          <tr className="flex gap-2 items-center" key={idx}>
+            <td className="text-[#334155] text-xs font-normal">{chi}</td>
             {idx !== 3 ? <Image src={DoubledropIcon} alt="arrow icon" /> : null}
           </tr>
         ))}
